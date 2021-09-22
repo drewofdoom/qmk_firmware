0b4bf5b8ae349994e908b0a52ee7b9234149bd26
Drew DeVore <drew@devorcula.com>
2021-09-22 08:47:20 -0600

    blush pink

--- keyboards/massdrop/alt/keymaps/drewofdoom/config.h
+++ keyboards/massdrop/alt/keymaps/drewofdoom/config.h
@@ -129,7 +129,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
 #define RGB_MATRIX_STARTUP_HUE 218
 #define RGB_MATRIX_STARTUP_SPD 115
-#define RGB_MATRIX_STARTUP_SAT 255
+#define RGB_MATRIX_STARTUP_SAT 203
 #define RGB_MATRIX_STARTUP_VAL 255
 
 #include "config_led.h"
